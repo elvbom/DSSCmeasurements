7e5b28ebfa019092381bae46ff55b79b8d72afbd
elvbom <henri3@Henri-till-Henri.local>
2017-02-27 16:21:46 -0500

    mer mat

--- pv_measurements.m
+++ pv_measurements.m
@@ -280,23 +280,25 @@ for i=1:20
     pmax15(i) = NaN;
 end
 
-pmax15(5) = am_51(1);  %fel?: 20.41
+pmax15(5) = 15.53;  %fel?: 20.41
 pmax15(10) = 15.94;
 pmax15(20) = 27.21;
 
 pmax20 = pmax15;
-pmax20(5) = am_51(2);
+pmax20(5) = 10.56;
 
 pmax25 = pmax15;
-pmax25(5) = am_51(3);
+pmax25(5) = 18.05;
 
 figure
 subplot(2, 1, 1)
-plot(konc, pmax15, '*-', konc, pmax20, 'x-', konc, pmax25, 'o-')
+plot(konc, pmax15, '*', konc, pmax20, 'x', konc, pmax25, 'o')
+xlim([0 20])
 ylim([0 30])
-title('5:1 amount vs Pmax')
+title('5:1 konc vs Pmax, 10:1 20:1 w/ 25')
 subplot(2, 1, 2)
 plot(corn_am, am_51)
+title('Amount vs Pmax')
 
 % figure %5:1, max utan fel 18.05, nr2 15.53
 % subplot(2, 1, 1)
