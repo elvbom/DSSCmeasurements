6c460110f5b2794f0e0b1ba9ea1a5fc68d6db4d9
elvbom <henri3@Henri-till-Henri.local>
2017-02-28 14:47:12 -0500

    pmax

--- pv_corn_1st_n_2nd_IV_pmax.m
+++ pv_corn_1st_n_2nd_IV_pmax.m
@@ -35,7 +35,7 @@ ylim([0 700])
 %efficiency w/ measurement errors
 figure
 %1:1
-subplot(6, 1, 1)
+subplot(5, 1, 1)
 plot(m113_I, m113_P, mX1_I, mX1_P, mX2_I, mX2_P, mXs1_I, mXs1_P, mXs2_I, mXs2_P, '-*', [0 200], [89.49 89.49], 'k')
 title('P 1st + 2nd maize I-Pmax w/ pot error curves, 1:1')
 xlim([0 0.55])
@@ -43,35 +43,28 @@ ylim([0 200])
 legend('11', 'X1', 'X2', 'Xs1', 'Xs2', 'Pmax 89.49')
 ylabel('Efficiency P')
 %2:1
-subplot(6, 1, 2)
+subplot(5, 1, 2)
 plot(m212_I, m212_P, mY1_I, mY1_P,  '-*', mY2_I, mY2_P, mYs1_I, mYs1_P, mYs2_I, mYs2_P, '-*', [0 200], [169.9 169.9], 'k', [0 200], [121.2 121.2], 'k')
 title('P 2nd maize 2:1')
 xlim([0 0.55])
 ylim([0 200])
 legend('21', 'Y1', 'Y2', 'Ys1', 'Ys2', 'Pmax? 169.9', 'Pmax 121.2')
 %4:1
-subplot(6, 1, 3)
+subplot(5, 1, 3)
 plot(m412_I, m412_P, mZ1_I, mZ1_P, mZ2_I, mZ2_P, [0 200], [113 113], 'k') 
 title('P 1st + 2nd maize 4:1')
 xlim([0 0.55])
 ylim([0 200])
 legend('1', '1Z', '2Z', 'Pmax 113')
 %5:1
-subplot(6, 1, 4)
+subplot(5, 1, 4)
 plot(m511_I, m511_P, '-*', m512_I, m512_P, m511_15_I, m511_15_P, m512_15_I, m512_15_P, [0 200], [148.4 148.4], 'k', [0 200], [89.51 89.51], 'k') %en f?r d?lig
 title('P 2nd maize 5:1')
 xlim([0 0.55])
 ylim([0 200])
 legend('Pmax? 148.4', 'Pmax 89.51')
 %10:1
-subplot(6, 1, 5)
-plot(m1011_I, m1011_P, m1012_I, m1012_P, m1013_I, m1013_P,  '-*', [0 200], [163.4 163.4], 'k', [0 200], [103.1 103.1], 'k')
-title('P 2nd maize 10:1')
-xlim([0 0.55])
-ylim([0 200])
-legend('Pmax? 163.4', 'Pmax 103.1')
-%20:1
-subplot(6, 1, 6)
+subplot(5, 1, 5)
 plot(m1011_I, m1011_P, m1012_I, m1012_P, m1013_I, m1013_P,  '-*', [0 200], [163.4 163.4], 'k', [0 200], [103.1 103.1], 'k')
 title('P 2nd maize 10:1')
 xlim([0 0.55])
--- pv_corn_tot_konc_vs_pmax_plot.m
+++ pv_corn_tot_konc_vs_pmax_plot.m
@@ -1,3 +1,5 @@
+close all
+
 konc = 1:1:20;
 
 %konc vs Pmax
@@ -6,19 +8,14 @@ for i=1:20
     pmax(i) = NaN;
 end
 
-%med LED
+%Pmax med LED
 %1:1 : 89.49, 
 %2:1 : 121.2
 %4:1 : 113
 %5:1 : 89.51/148.4
 %10:1 : 103.1/163.4
 
-%med overhead
-%5:1 :  15g     20g     25g
-%10:1 : 
-%20:1 : 
-
-%2
+%1&2
 pmax(1) = 89.49;
 pmax(2) = 121.2;
 pmax(4) = 113;
@@ -29,10 +26,16 @@ pmax_meas = pmax;
 pmax_meas(5) = 148.4;
 pmax_meas(10) = 163.4;
 
+%Pmax med overhead
+%5:1 :  15g     20g     25g
+%10:1 : 
+%20:1 : 
+
 %3
-corn_am = [0 5 10 15 20 25];
+corn_am = [0 5 10 15 20 25]; %alla 5:1
 am_51 = [NaN NaN NaN 15.53 10.56 18.05];
 
+pmax15 = pmax;
 pmax15(5) = 15.53;  %fel?: 20.41
 pmax15(10) = 15.94;
 pmax15(20) = 27.21;
@@ -54,6 +57,46 @@ plot(konc, pmax15, '*', konc, pmax20, 'x', konc, pmax25, 'o')
 xlim([0 20])
 ylim([0 30])
 title('5:1 konc vs Pmax, 10:1 20:1 w/ 25')
-subplot(2, 1, 2)
+subplot(3, 1, 3)
 plot(corn_am, am_51)
 title('Amount vs Pmax')
+
+%en plott med alla corn pmax inlagda
+%(1+2 default)
+%1:1
+    %13.32
+    %88.89 (fel?)
+    %59.66
+    %65.92
+    %89.49
+%2:1
+    %63.1
+    %72.62
+    %126.9 (fel?)
+    %169.9
+    %121.2
+%4:1
+    %113
+    %94.34
+    %66.92
+%5:1
+    %89.51 
+    %148.4 (fel?)
+    %1.246 (15g overhead)
+    %15.53 (dito)
+    %22.66 (dito, fel?)
+    %10.56 (20g overhead)
+    %8.822 (dito)
+    %7.489 (dito)
+    %14.29 (25g overhead)
+    %18.05 (dito)
+    %22.66 (dito)
+%10:1
+    %163.4 (fel?)
+    %103.1
+    %84.91
+    %15.94 (25g overhead)
+%20:1 (25g overhead)
+    %27.21
+    %20.4
+    %16.61
