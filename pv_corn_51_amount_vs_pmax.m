742f45302c65e16899924c067d540eb4c8ae8341
elvbom <henri3@Henri-till-Henri.local>
2017-02-28 12:12:37 -0500

    delar upp script, konc for sig

---
+++ pv_corn_51_amount_vs_pmax.m
@@ -0,0 +1,31 @@
+clear
+close all
+
+konc = 1:1:20;
+corn_am = [0 5 10 15 20 25];
+am_51 = [NaN NaN NaN 15.53 10.56 18.05];
+
+pmax15 = zeros(1, 20);
+for i=1:20
+    pmax15(i) = NaN;
+end
+
+pmax15(5) = 15.53;  %fel?: 20.41
+pmax15(10) = 15.94;
+pmax15(20) = 27.21;
+
+pmax20 = pmax15;
+pmax20(5) = 10.56;
+
+pmax25 = pmax15;
+pmax25(5) = 18.05;
+
+figure
+subplot(2, 1, 1)
+plot(konc, pmax15, '*', konc, pmax20, 'x', konc, pmax25, 'o')
+xlim([0 20])
+ylim([0 30])
+title('5:1 konc vs Pmax, 10:1 20:1 w/ 25')
+subplot(2, 1, 2)
+plot(corn_am, am_51)
+title('Amount vs Pmax')
---
+++ pv_corn_konc_vs_pmax.m
@@ -0,0 +1,24 @@
+close all
+clear
+
+konc = 1:1:20;
+
+%konc vs Pmax
+pmax_meas = zeros(1, 20);  %m matfel
+for i=1:20
+    pmax_meas(i) = NaN;
+end
+
+pmax_meas(1) = 89.49;
+pmax_meas(2) = 121.2;
+pmax_meas(4) = 113;
+pmax_meas(5) = 148.4;
+pmax_meas(10) = 163.4;
+pmax = pmax_meas;
+pmax(5) = 89.51;
+pmax(10) = 103.1;
+
+pmax3 = pmax_meas;
+
+figure
+plot(konc, pmax_meas, '*-', konc, pmax, '*-');
